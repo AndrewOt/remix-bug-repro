bd8a0e40453296c98380af3a34da42581c239ab3
Andrew <aottavianospam@outlook.com>
2023-12-08 07:45:53 -0600

    added page routes too

---
+++ app/routes/my-page2.$theId.tsx
@@ -0,0 +1,28 @@
+import type { LoaderFunctionArgs } from "@remix-run/node";
+import { redirect } from "@remix-run/node";
+import { Form, useActionData } from "@remix-run/react";
+
+export async function action({ params, request }: LoaderFunctionArgs) {
+  const { theId } = params;
+  const formData = await request.formData();
+  const name = formData.get("name");
+  if (!name) {
+    return { error: "Name is required" };
+  }
+  console.log(name);
+  return redirect(`/my-page2/${theId}/success`);
+}
+
+export default function MyPage() {
+  const data = useActionData() as { error: string };
+
+  return (
+    <>
+      <Form method="post">
+        <input type="text" name="name" />
+        {data?.error ? <span>{data.error}</span> : null}
+        <input type="submit" />
+      </Form>
+    </>
+  );
+}
